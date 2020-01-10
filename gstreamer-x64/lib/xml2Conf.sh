2fa3fe551d953d5f9e33b02851e3068f7546e898
zhougang <1183901440@qq.com>
2020-01-10 10:18:46 +0800

    commit resource

---
+++ gstreamer-x64/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/c/gstreamer/1.0/x86_64/lib"
+XML2_LIBS="-lxml2 -Lc:/gstreamer/1.0/x86_64/lib -lz      "
+XML2_INCLUDEDIR="-I/c/gstreamer/1.0/x86_64/include/libxml2"
+MODULE_VERSION="xml2-2.9.7"
+
